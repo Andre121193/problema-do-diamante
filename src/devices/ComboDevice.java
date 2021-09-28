d64cc820e187597848bb77abd98a74cc08fae27d
Andre121193 <andre1993andre@hotmail.com>
2021-09-27 21:32:16 -0300

    problema do diamante

---
+++ src/devices/ComboDevice.java
@@ -0,0 +1,26 @@
+package devices;
+
+public class ComboDevice extends Device implements Scanner, Printer {
+
+	public ComboDevice(String serialNumber) {
+		super(serialNumber);
+	}
+
+	@Override
+	public void print(String doc) {
+		System.out.println("Combo printing: " + doc);
+
+	}
+
+	@Override
+	public String scan() {
+		return "combo scan result";
+	}
+
+	@Override
+	public void processDoc(String doc) {
+		System.out.println("Combo processing: " + doc);
+
+	}
+
+}
---
+++ src/devices/Scanner.java
@@ -0,0 +1,7 @@
+package devices;
+
+public interface Scanner {
+	
+	String scan();
+
+}
